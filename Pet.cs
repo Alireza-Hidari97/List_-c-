5de636008241ad0f124d43ceaef547bbdc557c49
Alireza Haidari <m.ghorbat@yahoo.com>
2021-12-23 17:55:15 -0500

    List in c#

---
+++ Pet.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetProject
+{
+    class Pet
+    {
+        public string Name { get; }
+
+        public string Owner { get; private set; }
+
+        public int Age { get; }
+
+        public string Description { get; }
+
+        public bool IsHouseTrained { get; private set; }
+
+        public Pet(string name,
+            int age,
+            string description)
+        {
+            Name = name;
+            Age = age;
+            Description = description;
+            Owner = "no one";
+            IsHouseTrained = false;
+        }
+
+        public override string ToString()
+        {
+            string fullDescription;
+
+            fullDescription = $"Pet's Name: {Name}, owner: {Owner}, age: {Age}, ";
+            fullDescription += $"the pet is{(IsHouseTrained ? "" : " not")} trained, {Description}";
+
+            return fullDescription;
+        }
+
+        public void SetOwner(string owner)
+        {
+            Owner = owner;
+        }
+
+        public void Train()
+        {
+            IsHouseTrained = true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            Pet myDog = new Pet("Daisy", 2, "easygoing beautiful poodle");
+            Pet myCat = new Pet("Fluffy", 1, "stubborn persian cat");
+            Pet myBird = new Pet("Blue", 3, "playful arara");
+            Pet myFish = new Pet("Nemo", 1, "silly clownfish");
+
+            //2
+            //List<Pet> myPets = new List<Pet>() { myDog, myCat, myBird, myFish };
+            List<Pet> myPets = new List<Pet>();
+
+            myPets.Add(myDog);
+            myPets.Add(myCat);
+            myPets.Add(myBird);
+            myPets.Add(myFish);
+
+            //3
+            myDog.Train();
+            myCat.Train();
+
+            myCat.SetOwner("Jane");
+            myBird.SetOwner("John");
+            myFish.SetOwner("Jane");
+
+            //4
+            foreach (Pet p in myPets)
+                Console.WriteLine(p.ToString());
+
+            //5
+            Console.WriteLine("-------------------------------");
+            Console.Write("Find pets that belong to: ");
+            string owner = Console.ReadLine();
+
+            bool found = false;
+            foreach (Pet p in myPets)
+                if (p.Owner.ToUpper() == owner.ToUpper())
+                {
+                    Console.WriteLine(p.ToString());
+                    found = true;
+                }
+
+            if (!found)
+                Console.WriteLine($"No pets found belonging to {owner}");
+
+            Console.WriteLine("-------------------------------");
+
+        }
+    }
+}
